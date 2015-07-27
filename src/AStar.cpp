d1549cd66af3ed6ee0f62758a74941ecf061c94f
Kiliagan <kiliansullivan26@gmail.com>
2015-07-27 10:45:37 +0100

    Planning Ahead

---
+++ src/AStar.cpp
@@ -0,0 +1,18 @@
+/*
+ * AStar.cpp
+ *
+ *  Created on: 27 Jul 2015
+ *      Author: root
+ */
+
+#include "AStar.h"
+
+AStar::AStar() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AStar::~AStar() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/AStar.h
@@ -0,0 +1,17 @@
+/*
+ * AStar.h
+ *
+ *  Created on: 27 Jul 2015
+ *      Author: root
+ */
+
+#ifndef ASTAR_H_
+#define ASTAR_H_
+
+class AStar {
+public:
+	AStar();
+	virtual ~AStar();
+};
+
+#endif /* ASTAR_H_ */
